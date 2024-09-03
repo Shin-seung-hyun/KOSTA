a2bdc2ae3c76ecca5d1972198357dfb76f06b5fb
Shin-seung-hyun <jjack1028@naver.com>
2024-09-04 00:08:24 +0900

    add : [WEB] workshop09

--- work05_BackEnd/workshop09/src/main/java/com/edu/Kickboard.java
+++ work05_BackEnd/workshop09/src/main/java/com/edu/Kickboard.java
@@ -1,58 +1,73 @@
 package com.edu;
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
 import java.util.StringTokenizer;
-
-import org.apache.tomcat.websocket.WsRemoteEndpointImplBase;
 
 public class Kickboard {
 
 		static int M;
 		static int N;
 		static int[][] map;
-		static boolean[][] visited;
+		static boolean[][] visited;    
+	    static int dir[][] = { { 0,-1},{0,1},{1,0},{-1,0}};
+	    static int cnt =0;
 		
-		static int dir[][] = { { 0,-1},{0,1},{1,0},{-1,0}};
-		
-		public static int execute(File path) throws IOException , FileNotFoundException {
-			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    public static int execute(File path) throws IOException {
+	        BufferedReader br = new BufferedReader(new FileReader(path));
 	        StringTokenizer st = new StringTokenizer(br.readLine());
-			
-	        M = Integer.parseInt(st.nextToken());
+
 	        N = Integer.parseInt(st.nextToken());
-	        
-	        map = new int [M+1][N+1];
-	        visited = new boolean[M+1][N+1];
-	        
-	        for(int i=1; i<=M; i++) {
-	        	st = new StringTokenizer(br.readLine());
-	        	for(int j=1; j<=N; j++) {
-	        		map[i][j] = Integer.parseInt(st.nextToken());
-	        	}
+	        M = Integer.parseInt(st.nextToken());
+
+	        map = new int [N+1][M+1];
+	        visited = new boolean[N+1][M+1];
+
+	        for(int i=1; i<=N; i++) {
+	            st = new StringTokenizer(br.readLine());
+	            for(int j=1; j<=M; j++) {
+	                map[i][j] = Integer.parseInt(st.nextToken());
+	            }
 	        }
-	        
-	        int cnt =0;
-	        
-	        DFS(1, 1);
-			
-			return cnt;
-		}
-		
-		public static void DFS(int x, int y) {
-			
-			
-		}
-		
-		
-		public static void main(String[] args) throws IOException {
-			System.out.println(execute(new File("input.txt")));
-			
-		}
+
+	        DFS(1, 1, map[1][1]);
+
+	        return cnt;
+	    }
+
+	    public static void DFS(int x, int y, int current) {
+
+	        for(int i=0; i<4; i++){
+
+	            int nx = x + dir[i][0];
+	            int ny = y + dir[i][1];
+
+	            if( nx == N && ny ==M ){
+
+	                cnt = cnt +1;
+	                //System.out.println();
+
+	                return ;
+	            }
+	            else if( nx >N || nx <=0 || ny >M || ny <=0) continue;
+	            else if (map[nx][ny] >= current) continue;
+	            else if (visited[nx][ny]) continue;
+	            else {
+
+	                visited[nx][ny] = true;
+	                //System.out.println(map[nx][ny]);
+	                DFS(nx, ny, map[nx][ny]);
+	                visited[nx][ny] = false;
+	            }
+	        }
+
+	    }
+
+	    public static void main(String[] args) throws IOException {
+	    	
+	        System.out.println(execute(new File("src/input.txt")));
+
+	    }
 
 }
