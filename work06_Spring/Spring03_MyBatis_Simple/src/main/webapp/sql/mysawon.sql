e416668fd934f9e5ca009a535b66bacdc2df4ed4
Shin-seung-hyun <jjack1028@naver.com>
2024-10-01 19:48:06 +0900

    add : [Spring] MyBatis DI 연결 주석 추가

--- work06_Spring/Spring03_MyBatis_Simple/src/main/webapp/sql/mysawon.sql
+++ work06_Spring/Spring03_MyBatis_Simple/src/main/webapp/sql/mysawon.sql
@@ -7,3 +7,11 @@ create table mysawon(
 	hiredate date,
 	constraint mysawon_id_uq unique(id)
 );
+
+
+INSERT INTO users VALUES('user01','홍길동','user01',10,1,'2019-10-11'); 
+INSERT INTO users VALUES('user02','이순신','user02',20,2, '2019-10-12');
+INSERT INTO users VALUES('user03','김유신','user03',30,3, '2019-10-09'); 
+INSERT INTO users VALUES('mybatis01','홍길동iba','mybatis01',10,1,'2019-10-08');
+INSERT INTO users VALUES('mybatis02','이순신iba','mybatis02',20,2, '2019-10-07');
+INSERT INTO users VALUES('mybatis03','김유신iba','mybatis03',30,3,'2019-10-02');
